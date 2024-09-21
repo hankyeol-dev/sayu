2c6ada30a6b0ac664cf80afb7993ec29862bd027
hankyeol-dev <ddovlek@kakao.com>
2024-09-21 13:23:33 +0900

    [feature/live-sayu] 📱- Subcontent 작성 뷰 구현 및 코드 정리

---
+++ sayu/Components/FlexableTextView.swift
@@ -0,0 +1,8 @@
+//
+//  FlexableTextView.swift
+//  sayu
+//
+//  Created by 강한결 on 9/21/24.
+//
+
+import Foundation
---
+++ sayu/Components/SayuSubCreator.swift
@@ -0,0 +1,18 @@
+//
+//  SayuSubCreator.swift
+//  sayu
+//
+//  Created by 강한결 on 9/20/24.
+//
+
+import SwiftUI
+
+struct SayuSubCreator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SayuSubCreator()
+}
